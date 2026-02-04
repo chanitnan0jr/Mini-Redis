3a106f1947905b2aa26ca3f2d1fa5c593b17e0b3
chanitnan0jr <ch4n1tnan@gmail.com>
2026-02-04 22:29:51 +0700

    feat: add showip tool to demonstrate getaddrinfo usage

---
+++ Src/showip.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+/*this file contains code to lookup and display the all IP addresses for a given hostname
+usage: ./showip hostname
+example: ./showip www.google.com
+*/
+int main(int argc, char *argv[]){
+    struct addrinfo hints, *res, *p;
+    int status;
+    char ipstr[INET6_ADDRSTRLEN];
+
+    /*
+    argv[0] = "./showip" (ชื่อโปรแกรมจะอยู่ที่ช่อง 0 เสมอ)
+    argv[1] = "www.google.com" (ข้อมูลที่เราอยากได้จริงๆ)
+    argv[2] = NULL (จุดจบ บอกว่าหมดแล้ว)
+    */
+
+    if (argc != 2) {
+        fprintf(stderr,"usage: showip hostname\n");
+        return 1;
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;  // Either IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM;
+
+    /*
+    Port is NULL because we only want the IP address not connect to the server
+    */
+    if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+        return 2;
+    }
+
+    printf("IP addresses for %s:\n\n", argv[1]);
+
+    //
+    for(p = res;p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+        struct sockaddr_in *ipv4;
+        struct sockaddr_in6 *ipv6;
+
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+
+    freeaddrinfo(res); // free the linked list
+
+    return 0;
+}
